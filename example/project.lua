84c94b91a2eb35ac36832724f0eb443c697b922a
thepigcat <benpospo@gmail.com>
2023-09-23 01:01:17 +0200

    switch to rlua

--- example/project.lua
+++ example/project.lua
@@ -1,4 +1,4 @@
-local dependencies = {
+deps = {
     "test",
     "port"
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
-mod proj_cfg;
+extern crate rlua;
 
 use std::{fs::File, io::Read};
-use proj_cfg::ProjectHandler;
-use rslua::{parser::Parser, lexer::Lexer};
 
-fn main() {
-    let proj = ProjectHandler::new("example/project.lua");
+use rlua::{Lua, Result, Table};
 
-    let deps = proj.get_deps();
+fn main() -> Result<()> {
+    // Create a Lua runtime environment
+    let lua = Lua::new();
+
+    let mut file = File::open("example/project.lua").expect("Failed to open file");
+
+    // Lua code to evaluate
+    let mut lua_code = String::new();
+
+    file.read_to_string(&mut lua_code).expect("Failed to read file");
+
+    // Evaluate the Lua code
+    lua.context(|ctx| {
+        ctx.load(&lua_code).exec()?;
+
+        // Access the Lua table "my_table"
+        let my_table: Table = ctx.globals().get("deps")?;
+
+        // Now you have a Table containing the Lua table
+        // You can work with it as needed
+        for key in my_table.pairs::<String, String>() {
+            let (k, v) = key?;
+            println!("Key: {}, Value: {}", k, v);
+        }
+
+        Ok(())
+    })
 }
-
